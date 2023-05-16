70fc4904d01812190665432eea6ea9827706be06
Kiyoto Watanabe <kiyoto.watanabe@divx.co.jp>
2023-05-16 18:48:24 +0900

    output user's name in index

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,2 +1,18 @@
 class TweetsController < ApplicationController
+    def index
+        @tweets = Tweet.all
+        @users = User.all
+    end
+    def show
+    end
+    def new
+    end
+    def create
+    end
+    def edit
+    end
+    def destory
+    end
+    def update
+    end
 end
